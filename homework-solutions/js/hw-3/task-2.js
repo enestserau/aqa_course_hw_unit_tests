5ba314df45967c916653db12fe969937ea28a73e
evgenynesterov <nesterov.cosmic@gmail.com>
2025-07-25 12:59:49 +0200

    Task 1 and 2 completion

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,17 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+function sumConcatination(n) {
+    if (n >= 1 && n <= 9){
+    const str = String(n);
+    const nn = str + str;
+    const nnn = str + str + str;
+    const result = n + Number(nn) + Number(nnn)
+    console.log(`Result: ${n}+${nn}+${nnn}=${result}`)
+    }
+    else{
+        console.log("Please enter number between 1 and 9")
+    }
+}
+sumConcatination(3); // Enter number between 1 and 9
