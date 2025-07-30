0110beb7b60e0fa781fecd98c38e8529c28bb8da
evgenynesterov <nesterov.cosmic@gmail.com>
2025-07-30 20:27:29 +0300

    homework 4 tasks 1,2,3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,22 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;
+let age = "59";
+
+if (typeof(age) === "string") {
+  age = Number(age);
+}
+
+if (typeof(age) !== "number") {
+  console.log("Incorrect data type");
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+} else if (age <= maxAge && age >= minAge) {
+  console.log("Welcome");
+} else if (age > maxAge) {
+  console.log("Keep calm and watch culture channel");
+} else
+  console.log("Technical work");
