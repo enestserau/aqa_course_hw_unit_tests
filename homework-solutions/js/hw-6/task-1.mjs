87b827954097c9f0041854063caeb84f1f489b44
evgenynesterov <nesterov.cosmic@gmail.com>
2025-08-07 09:41:51 +0200

    hw-6 solutions 1,2,3

--- homework-solutions/js/hw-6/task-1.mjs
+++ homework-solutions/js/hw-6/task-1.mjs
@@ -8,6 +8,12 @@
   Значение добавьте в массив 'forOf' 
 */
 const forOf = [];
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (const num of numArr) {
+  forOf.push(num % 2 === 0 ? num ** 2 : num ** 3);
+}
+console.log(forOf);
 
 /*
 2. Методы массивов
@@ -20,7 +26,13 @@ const forOf = [];
   В результате вы должны получить массив [0, 1, 3, 4, 5], присвойте в переменную "result"
 */
 
-let result;
+let result = [1, 2, 3, 4, 5];
+result.push(6);
+result.unshift(0);
+result.splice(2, 1);
+result.pop();
+console.log(result);
+
 
 /*
 3. Деструктуризация массивов
@@ -30,6 +42,14 @@ let result;
   Пример: [1,2,3,4,5] => first === 1; second === 2, rest === [3,4,5]
 */
 
+const numbers = [3, 11, 32, 7, 20];
+
+const [first, second, ...rest] = numbers;
+
+console.log(first);
+console.log(second);
+console.log(rest);
+
 /*
 4. Конкатенация массивов
   - Создайте массив с числами [1,2,3,4,5]
@@ -38,5 +58,12 @@ let result;
 
   Создайте переменную mergedArray, который будет хранить значения из массивов 1 и 2
 */
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+
+const mergedArray = [...array1, ...array2];
+
+console.log(mergedArray);
 
 export { forOf, result, first, second, rest, mergedArray };
--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,44 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+//1. Test for myPizzasT1
+
 let resultUnique;
 let resultNull;
+
+const competitorPizzasLower = competitorPizzas.map(pizza => pizza.toLowerCase());
+const uniquePizzasT1 = [];
+
+for (const myPizza of myPizzasT1) {
+  if (!competitorPizzasLower.includes(myPizza.toLowerCase())) {
+    uniquePizzasT1.push(myPizza);
+  }
+}
+
+if (uniquePizzasT1.length > 0) {
+  resultUnique = uniquePizzasT1;
+} else {
+  resultNull = null;
+}
+
+//2. Test for myPizzasT2
+
+const uniquePizzasT2 = [];
+
+for (const myPizza of myPizzasT2) {
+  if (!competitorPizzasLower.includes(myPizza.toLowerCase())) {
+    uniquePizzasT2.push(myPizza);
+  }
+}
+
+if (uniquePizzasT2.length > 0) {
+  resultUnique = uniquePizzasT2;
+} else {
+  resultNull = null;
+}
 
 export { resultNull, resultUnique };
